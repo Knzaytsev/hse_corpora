590b9ab7c4648a0361ca327938df24da9391132a
Константин Зайцев <adugeen@MacBook-Pro-Konstantin.local>
2023-01-10 21:03:45 +0300

    change search engine by adding subqueries and joins

--- rest/app/api/search_engine.py
+++ rest/app/api/search_engine.py
@@ -1,6 +1,7 @@
 from app.db.models import TokenizedTexts, Sentences, Texts
 from app.db.utils import exec_statement
 from sqlalchemy import select, and_, or_
+from sqlalchemy.orm import aliased
 
 column_mapper = {
     'lemma': TokenizedTexts.lemma,
@@ -9,14 +10,52 @@ column_mapper = {
     'pos': TokenizedTexts.token_spacy_pos,
 }
 
+PREFIX = "tokenized_"
 
-def create_condition(forms):
-    return or_(
-        *[and_(*[column_mapper[condition].op("~")('^' + value + '$')
-                 for conditions in form['conditions']
-                 for condition, value in conditions.items()])
-          for form in forms]
-    )
+
+# 1. Create N search fields
+# 2. Join search fields
+# 3. Select from joined fields
+
+def create_condition(form):
+    return and_(*[column_mapper[condition].op("~")('^' + value + '$')
+                  for conditions in form['conditions']
+                  for condition, value in conditions.items()])
+
+
+def create_search_field(form):
+    search_field = select(TokenizedTexts.lemma, TokenizedTexts.token,
+                          TokenizedTexts.token_inx, TokenizedTexts.token_spacy_pos,
+                          TokenizedTexts.text_id, TokenizedTexts.text_year,
+                          TokenizedTexts.sentence_id, TokenizedTexts.task_id) \
+        .where(create_condition(form))
+
+    return search_field
+
+
+def join_fields(fields):
+    init_field = fields[0]
+    stmt = select(*fields)
+    for field in fields[1:]:
+        stmt = stmt.join(field, and_(init_field.c.text_id == field.c.text_id, init_field.c.text_year == field.c.text_year,
+                                     init_field.c.sentence_id == field.c.sentence_id, init_field.c.task_id == field.c.task_id))
+    return stmt
+
+
+def create_statement(forms):
+    fields = [aliased(create_search_field(form).subquery(), name=PREFIX + str(i)) for i, form in enumerate(forms)]
+    joined_fields = aliased(join_fields(fields).subquery(), name="fields")
+    stmt = select(Texts.text_name, joined_fields, Sentences.sentence_tokens) \
+                .join(Sentences, and_(Texts.text_id == Sentences.text_id,
+                              Texts.text_year == Sentences.text_year,
+                              Texts.task_id == Sentences.task_id)) \
+                .join(joined_fields,
+                        and_(Sentences.text_id == joined_fields.c.text_id,
+                   Sentences.text_year == joined_fields.c.text_year,
+                   Sentences.sentence_id == joined_fields.c.sentence_id,
+                   Sentences.task_id == joined_fields.c.task_id))
+    return stmt
+
 
 def get_token_span(sentence, token_inx, token):
     sentence = sentence.split(' ')
@@ -24,33 +63,23 @@ def get_token_span(sentence, token_inx, token):
     token_start = token_end - len(token)
     return token_start, token_end
 
-def exec_search(forms):
-    stmt = select(Texts.text_name, TokenizedTexts.lemma, TokenizedTexts.token, 
-                  TokenizedTexts.token_inx, TokenizedTexts.token_spacy_pos, Sentences.sentence_tokens,
-                  ) \
-        .join(Sentences, and_(Texts.text_id == Sentences.text_id,
-                              Texts.text_year == Sentences.text_year, 
-                              Texts.task_id == Sentences.task_id)) \
-        .join(TokenizedTexts,
-              and_(Sentences.text_id == TokenizedTexts.text_id,
-                   Sentences.text_year == TokenizedTexts.text_year,
-                   Sentences.sentence_id == TokenizedTexts.sentence_id, 
-                   Sentences.task_id == TokenizedTexts.task_id)) \
-        .where(create_condition(forms))
 
-    descriptions = stmt.column_descriptions
+def exec_search(forms):
+    stmt = create_statement(forms)
+
     result = exec_statement(stmt)
 
     rows = list()
     for row in result:
-        columns = dict()
-        for i, description in enumerate(descriptions):
-            column, value = description["name"], row[i]
-            columns[column] = value
+        row = dict(row)
 
-        token_start, token_end = get_token_span(columns['sentence_tokens'], columns['token_inx'], columns['token'])
-        columns['token_start'] = token_start
-        columns['token_end'] = token_end
+        for i in range(len(forms)):
+            postfix = '_' + str(i) if i > 0 else ''
 
-        rows.append(columns)
+            token_start, token_end = get_token_span(
+                row['sentence_tokens'], row['token_inx' + postfix], row['token' + postfix])
+            row['token_start' + postfix] = token_start
+            row['token_end' + postfix] = token_end
+
+        rows.append(row)
     return rows
