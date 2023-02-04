f65810e59d734d5287798431d673344dcc2a75ce
Константин Зайцев <adugeen@MacBook-Pro-Konstantin.local>
2023-02-04 16:28:44 +0300

    add search by mistakes

--- rest/app/api/controllers.py
+++ rest/app/api/controllers.py
@@ -1,24 +1,30 @@
-from app.api.constants import MULTIPLE_COLUMNS, POSTFIX_PATTERN, MODEL_FIELDS, NUM_FIELDS
-from app.api.search_engine import exec_search
+from app.api.constants import WORDS_COLUMNS, POSTFIX_PATTERN, MODEL_FIELDS, NUM_FIELDS
+from app.api.search_module.search_engine import exec_search
 
 
 def filter_fields(row):
     output = dict()
     for field in MODEL_FIELDS:
-        output[field] = row[field]
+        if field in row:
+            output[field] = row[field]
     return output
 
 def controller_exec_search(forms):
     result = exec_search(forms)
 
     for row in result:
-        for column in MULTIPLE_COLUMNS:
+        for column in WORDS_COLUMNS:
             column_vals = []
-            for i in range(len(forms)):
-                column_vals.append(row.pop(column + POSTFIX_PATTERN(i)))
+            offset = 0
+            for i, form in enumerate(forms):
+                if form['by'] in ['mistake']:
+                    offset += 1
+                    continue
+                column_vals.append(row.pop(column + POSTFIX_PATTERN(i - offset)))
             row[column] = column_vals
+
         row[NUM_FIELDS] = len(forms)
-    
+
     result = [filter_fields(row) for row in result]
 
     return result
