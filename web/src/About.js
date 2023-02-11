15fdd4634574df2a942ea1f65c7702c68cb87bf9
Daria <darianow112@gmail.com>
2023-02-11 00:21:46 +0000

    Add about page

---
+++ web/src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>Realec Search</h1>
+      <p>This is a tool to search Realec.</p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About</title>
+
+export default AboutPage
